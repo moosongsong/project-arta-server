3265115855f3a609c3f1c51ee9e69087a7d3e3c7
moosongsong <songe08@gmail.com>
2021-05-03 16:39:01 +0900

    add media url

--- arta_project/urls.py
+++ arta_project/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('exhibition/', include('exhibition.urls')),
     path('', include('single_page.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
