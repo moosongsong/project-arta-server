601360c52ec174dc6274b4d457a8bdafa2112046
moosongsong <songe08@gmail.com>
2021-05-08 17:25:31 +0900

    add sidebar

--- exhibition/urls.py
+++ exhibition/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # path('about_me/', views.about_me),
-    # path('', views.landing),
+    path('', views.ExhibitionList.as_view()),
 ]
--- exhibition/views.py
+++ exhibition/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Exhibition
 
-# Create your views here.
+
+class ExhibitionList(ListView):
+    model = Exhibition
+    ordering = '-pk'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super(ExhibitionList, self).get_context_data()
+        # context['categories'] = Category.objects.all()
+        # context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
