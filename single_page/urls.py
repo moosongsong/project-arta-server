2682a41b23f8abf7fa550712e4b85ad742bbc6b3
moosongsong <songe08@gmail.com>
2021-05-14 16:12:46 +0900

    delete single app

--- single_page/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('about/', views.about_us),
-    path('login/', views.login),
-    path('mypage/', views.mypage),
-    path('', views.landing),
-]
